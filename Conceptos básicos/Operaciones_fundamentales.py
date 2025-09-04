e0a558d101628c577bf30a9696191282fc70a135
Jacksonbill3108 <jangulowoqr@gmail.com>
2025-09-03 21:00:13 -0400

    Avance con el grupo E1, gestion 2-2025

---
+++ Conceptos básicos/Operaciones_fundamentales.py
@@ -0,0 +1,8 @@
+#Datos
+dato = int(input("Ingrese un dato: "))
+dato_2 = int(input("Ingrese otro dato: "))
+print(dato + dato_2) 
+print(dato - dato_2)
+print(dato * dato_2)
+print(dato / dato_2)
+#2.14
---
+++ Conceptos básicos/concatenacion.py
@@ -0,0 +1,7 @@
+dato_a = input("Introducir dato inicial a: ")
+dato_b = input("Introducir dato inicial b: ")
+print(f'{dato_a} {dato_b} \nTermino el programa')
+dato_c = float(input("Ingrese un numero: "))
+print(dato_c)
+print(2.14 + dato_c)
+
