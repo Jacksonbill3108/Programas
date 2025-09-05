2c41fb27eb3ee0ba69a22766187f8ed5a1b1449e
Jacksonbill3108 <jangulowoqr@gmail.com>
2025-09-05 17:16:53 -0400

    Ejercicio 1 E2

---
+++ Conceptos básicos/Calculo_distancia.py
@@ -0,0 +1,7 @@
+lista = []
+print("[x1,y1,x2,y2]")
+for i in range(4):
+    dato = int(input("Ingrese un punto: "))
+    lista.append(dato)
+distancia = ((lista[2]-lista[0])**2+(lista[3]-lista[1])**2)**1/2
+print("La distancia entre punto 1 y punto 2 es: ", distancia)
